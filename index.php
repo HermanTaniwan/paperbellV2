ecb7b5b2330c6f5b3487a8e080dfefd6eb80c668
hermantaniwan <taniwanherman@yahoo.com>
2026-08-28 19:58:28 +0700

    Refresh cached print status script

--- index.php
+++ index.php
@@ -1510,7 +1510,7 @@ window.PAPERBELL_CONFIG = <?= json_encode(['authEnabled' => (bool)($config['auth
 </div>
 <script src="assets/vue.global.prod.js">
 </script>
-<script src="assets/app.js?v=109">
+<script src="assets/app.js?v=110">
 </script>
 </body>
 </html>
