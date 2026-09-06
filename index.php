b8d852128e3ee86b2833a9485a41cf2ba41ddd1a
hermantaniwan <taniwanherman@yahoo.com>
2026-09-07 04:58:01 +0700

    Muat ulang logika antrean cetak

--- index.php
+++ index.php
@@ -1523,7 +1523,7 @@ window.PAPERBELL_CONFIG = <?= json_encode(['authEnabled' => (bool)($config['auth
 </div>
 <script src="assets/vue.global.prod.js">
 </script>
-<script src="assets/app.js?v=119">
+<script src="assets/app.js?v=120">
 </script>
 </body>
 </html>
