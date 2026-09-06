743a419a65ca10eaa9ec58c0ed8bd52c03c7e8aa
hermantaniwan <taniwanherman@yahoo.com>
2026-09-07 06:03:28 +0700

    Tampilkan progres nol saat printer mulai

--- index.php
+++ index.php
@@ -1531,7 +1531,7 @@ window.PAPERBELL_CONFIG = <?= json_encode(['authEnabled' => (bool)($config['auth
 </div>
 <script src="assets/vue.global.prod.js">
 </script>
-<script src="assets/app.js?v=129">
+<script src="assets/app.js?v=130">
 </script>
 </body>
 </html>
