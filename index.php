01faa73640e208ca48d73e650eda3c2060748087
hermantaniwan <taniwanherman@yahoo.com>
2026-09-14 20:20:58 +0700

    Bump stock management script version

--- index.php
+++ index.php
@@ -1572,7 +1572,7 @@ window.PAPERBELL_CONFIG = <?= json_encode(['authEnabled' => (bool)($config['auth
 </div>
 <script src="assets/vue.global.prod.js">
 </script>
-<script src="assets/app.js?v=141">
+<script src="assets/app.js?v=142">
 </script>
 </body>
 </html>
